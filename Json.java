1fcec60c5a1a3f067310f649fcf7eea03ed344c6
michaelgarciam1 <mgarciamoreira@cifpfbmoll.eu>
2023-12-03 20:30:51 +0100

    ole

---
+++ Json.java
@@ -0,0 +1,25 @@
+import java.io.File;
+
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+public class Json {
+    public static void main(String[] args) {
+        try {
+            File xmlFile = new File("bookings.xml");
+            XmlMapper xmlMapper = new XmlMapper();
+            JsonNode jsonNode = xmlMapper.readTree(xmlFile);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonNode);
+            System.out.println(json);
+
+            File jsonFile = new File("bookings.json");
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(jsonFile, jsonNode);
+            System.out.println("Json file created");
+        } catch (Exception e) {
+            System.out.println(e);
+
+        }
+    }
+}
