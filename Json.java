6f056fe3aae94bed4d064a50d6ae6817e18b87de
michaelgarciam1 <mgarciamoreira@cifpfbmoll.eu>
2023-12-03 21:00:27 +0100

    algo mejor

--- Json.java
+++ Json.java
@@ -3,23 +3,26 @@ import java.io.File;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
-public class Json {
+
+
+public class Json{
     public static void main(String[] args) {
-        try {
-            File xmlFile = new File("bookings.xml");
+        try{
+            File xmlFile = new File("noExiste.xml");
             XmlMapper xmlMapper = new XmlMapper();
             JsonNode jsonNode = xmlMapper.readTree(xmlFile);
 
             ObjectMapper objectMapper = new ObjectMapper();
-            String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonNode);
+            String json =  objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonNode);
             System.out.println(json);
 
             File jsonFile = new File("bookings.json");
             objectMapper.writerWithDefaultPrettyPrinter().writeValue(jsonFile, jsonNode);
             System.out.println("Json file created");
-        } catch (Exception e) {
+        }catch(NullPointerException e){
+            System.out.println("No existe el archivo");
+        }catch(Exception e){
             System.out.println(e);
-
         }
     }
 }
